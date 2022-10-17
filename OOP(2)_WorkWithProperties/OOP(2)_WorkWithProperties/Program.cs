a3f2f207e4fb9ea8f2fe464f0ccc1f674431cb74
AvanMyers <smail1341@gmail.com>
2022-10-17 15:58:06 +0300

    jobDone

---
+++ OOP(2)_WorkWithProperties/OOP(2)_WorkWithProperties/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2__WorkWithProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer render = new Renderer();
+            Player player = new Player(10, 10);
+
+            render.DrowPlayer(player.PlayerX, player.PlayerY);
+            Console.ReadKey();
+        }
+    }
+
+    class Renderer
+    {
+        public void DrowPlayer(int x, int y, char player = '+')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(player);
+        }
+    }
+
+    class Player
+    {
+        public int PlayerX { get; private set; }
+
+        public int PlayerY { get; private set; }
+
+        public Player(int playerX, int playerY)
+        {
+            PlayerX = playerX;
+            PlayerY = playerY;
+        }
+        
+    }
+}
